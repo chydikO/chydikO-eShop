4b818d0e2b56562e81239a50f9574cc3d135a21a
Oleg Chudnovskiy <oleg.chudnovskiy@gmail.com>
2020-06-29 16:54:37 +0300

    add RegistrationController

---
+++ eShop/Scene/ViewControllers/RegistrationController.swift
@@ -0,0 +1,30 @@
+//
+//  RegistrationController.swift
+//  eShop
+//
+//  Created by Oleg Chudnovskij on 29.06.2020.
+//  Copyright © 2020 Oleg Chudnovskij. All rights reserved.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
